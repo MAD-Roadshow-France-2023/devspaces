0f8adaa5ceaf79568819b9d5c5e3e1c48ddfe1d2
Nicolas Massé <nicolas.masse@itix.fr>
2023-06-04 14:44:27 +0200

    wip

--- src/main/resources/import-prod.sql
+++ src/main/resources/import-prod.sql
@@ -1,3 +1,10 @@
+INSERT INTO fruitEntity(id, name) VALUES (nextval('hibernate_sequence'), 'Cerise');
+INSERT INTO fruitEntity(id, name) VALUES (nextval('hibernate_sequence'), 'Pomme');
+INSERT INTO fruitEntity(id, name) VALUES (nextval('hibernate_sequence'), 'Poire');
+INSERT INTO fruitEntity(id, name) VALUES (nextval('hibernate_sequence'), 'Pêche');
+INSERT INTO fruitEntity(id, name) VALUES (nextval('hibernate_sequence'), 'Raisin');
+
+-- force using the same if for entity and repository to facilitate testing
 INSERT INTO fruit(id, name) VALUES (1, 'Cerise');
 INSERT INTO fruit(id, name) VALUES (2, 'Pomme');
 INSERT INTO fruit(id, name) VALUES (3, 'Poire');
