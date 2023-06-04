1769dc0259d2cbba5631c81f5832cf379e83c67a
Nicolas Massé <nicolas.masse@itix.fr>
2023-06-04 14:19:03 +0200

    sample data

---
+++ src/main/resources/import-prod.sql
@@ -0,0 +1,5 @@
+INSERT INTO fruit(id, name) VALUES (1, 'Ananas');
+INSERT INTO fruit(id, name) VALUES (2, 'Banane');
+INSERT INTO fruit(id, name) VALUES (3, 'Fruit de la passion');
+INSERT INTO fruit(id, name) VALUES (4, 'Goyave');
+INSERT INTO fruit(id, name) VALUES (5, 'Kaki');
---
+++ src/main/resources/import-test.sql
@@ -0,0 +1,5 @@
+INSERT INTO fruit(id, name) VALUES (1, 'Cerise');
+INSERT INTO fruit(id, name) VALUES (2, 'Pomme');
+INSERT INTO fruit(id, name) VALUES (3, 'Poire');
+INSERT INTO fruit(id, name) VALUES (4, 'Pêche');
+INSERT INTO fruit(id, name) VALUES (5, 'Raisin');
